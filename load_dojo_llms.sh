bf2f9cccb8d0fb9edac8ff47b1f27881b2c0a152
Mike Frager <mfrager@gmail.com>
2026-03-03 19:09:37 -0500

    Update LLMs

--- load_dojo_llms.sh
+++ load_dojo_llms.sh
@@ -2,6 +2,6 @@
 
 python -m dojo.scripts.populate_models \
   --provider "lmstudio/,http://host.docker.internal:8000/v1,http://localhost:8000" \
-  --provider "lmstudio_z/,http://bobs-mac.local:8000/v1,http://localhost:8000" \
+  --provider "lmstudio_z/,http://zincbook.local:8000/v1,http://localhost:8000" \
   --write-config
 
